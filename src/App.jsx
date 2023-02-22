31db6d0ea2b2a0336c53097b8d74e362927ba059
dylandhood <66035537+dylanhood@users.noreply.github.com>
2023-02-21 19:53:44 -0600

    Delete src directory

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-import Clicker from './Clicker.js'
-export default function App()
-{
-    return <>
-    <Clicker />
-    </>
-}
--- src/Clicker.js
+++
@@ -1,7 +0,0 @@
-export default function App()
-{
-    return <div>
-        <div>Clicks count: 0</div>
-        <button>Click Me</button>
-    </div>
-}
